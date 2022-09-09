d7b77255845de4cf3716af8ba21d6e393311b326
katendejericho5 <106674175+katendejericho5@users.noreply.github.com>
2022-09-09 09:55:52 +0300

    updates

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfoilio/pages/home/intro_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,25 +7,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          padding: const EdgeInsets.all(25.0), // 25 pixels in L, B, R, T
-          //color: Colors.blue,
-          alignment: Alignment.bottomCenter,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(.5),
-                  offset: Offset(0, 10),
-                  blurRadius: 15)
-            ],
-          ),
+      /*appBar: AppBar(
+        title: const Text('HELLO!'),
+      ),*/
+      body: Body(),
+    );
+  }
+}
 
-          child: const Text("Jericho"),
-        ),
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Welcome back ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w300),
+                          ),
+                          Text(
+                            "Jericho's Portfolio ",
+                            style: TextStyle(
+                                fontSize: 28, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Icon(
+                        Icons.account_circle,
+                        color: Colors.green,
+                        size: 30,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )),
       ),
     );
   }
